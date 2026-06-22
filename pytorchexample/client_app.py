7033ae2937c9defd8519068a6c3ba4fbaa939450
mohamedAdhamc <mohamed.adhamnagiub@gmail.com>
2026-06-22 16:11:08 +0200

    more pathological partioitoning

--- pytorchexample/client_app.py
+++ pytorchexample/client_app.py
@@ -4,7 +4,7 @@ import torch
 from flwr.app import ArrayRecord, Context, Message, MetricRecord, RecordDict
 from flwr.clientapp import ClientApp
 
-from pytorchexample.task import Net, load_data, load_nonIID_data
+from pytorchexample.task import Net, load_data, load_nonIID_data, load_nonIID_data_oneclassperpartition
 from pytorchexample.task import test as test_fn
 from pytorchexample.task import train as train_fn
 
@@ -28,7 +28,7 @@ def train(msg: Message, context: Context):
     batch_size = context.run_config["batch-size"]
     # proximal_mu = context.run_config["proximal-mu"]
     percent_stragglers: float = context.run_config["percent-stragglers"]
-    trainloader, _ = load_nonIID_data(partition_id, num_partitions, batch_size)
+    trainloader, _ = load_nonIID_data_oneclassperpartition(partition_id, num_partitions, batch_size)
 
     # Call the training function
     train_loss = train_fn(
@@ -66,7 +66,7 @@ def evaluate(msg: Message, context: Context):
     partition_id = context.node_config["partition-id"]
     num_partitions = context.node_config["num-partitions"]
     batch_size = context.run_config["batch-size"]
-    _, valloader = load_nonIID_data(partition_id, num_partitions, batch_size)
+    _, valloader = load_nonIID_data_oneclassperpartition(partition_id, num_partitions, batch_size)
 
     # Call the evaluation function
     eval_loss, eval_acc = test_fn(
--- pytorchexample/task.py
+++ pytorchexample/task.py
@@ -87,6 +87,28 @@ def load_nonIID_data(partition_id: int, num_partitions: int, batch_size: int):
     testloader = DataLoader(partition_train_test["test"], batch_size=batch_size)
     return trainloader, testloader
 
+def load_nonIID_data_oneclassperpartition(partition_id: int, num_partitions: int, batch_size: int):
+    """Load partition CIFAR10 data."""
+    # Only initialize `FederatedDataset` once
+    global fds
+    if fds is None:
+        partitioner = PathologicalPartitioner(num_partitions=num_partitions, partition_by="label", num_classes_per_partition=1, class_assignment_mode="first-deterministic")
+        fds = FederatedDataset(
+            dataset="uoft-cs/cifar10",
+            partitioners={"train": partitioner},
+        )
+    partition = fds.load_partition(partition_id)
+    # Divide data on each node: 80% train, 20% test
+    partition_train_test = partition.train_test_split(test_size=0.2, seed=42)
+    # Construct dataloaders
+    partition_train_test = partition_train_test.with_transform(apply_transforms)
+    trainloader = DataLoader(
+        partition_train_test["train"], batch_size=batch_size, shuffle=True
+    )
+    testloader = DataLoader(partition_train_test["test"], batch_size=batch_size)
+    return trainloader, testloader
+
+
 def load_centralized_dataset():
     """Load test set and return dataloader."""
     # Load entire test set
